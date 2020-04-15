2c9f6d847124d0bd133473ee136a1d814076dfbb
danushka96 <danushkaherath96@gmail.com>
2020-04-15 22:11:05 +0530

    added more file types

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,51 +1,15 @@
 <template>
     <div>
-        <v-content>
-            <v-container>
-                <video-card
-                        :key="movie._id"
-                        :movie="movie"
-                        :type="type"
-                        v-for="movie of movies">
-                </video-card>
-            </v-container>
-        </v-content>
-        <v-btn
-                bottom
-                color="pink"
-                dark
-                fab
-                fixed
-                right
-        >
-            <v-icon>mdi-plus</v-icon>
-        </v-btn>
+        <h1 style="margin-top: 100px; margin-left: 400px">Under Construction</h1>
     </div>
 </template>
 
 <script>
-    import VideoCard from "../components/VideoCard";
-    import http from "../plugins/http";
-
     export default {
-        components: {VideoCard},
-        props: {
-            source: String,
-        },
-        data: () => ({
-            movies: [],
-            type: 'movies'
-        }),
-        created() {
-            this.getMovies();
-        },
-        methods: {
-            getMovies() {
-                http.getFiles().then(result => {
-                    console.log(result);
-                    this.movies = result.data.filter(file => file.type === "movies")
-                })
-            }
-        }
+        name: "Home"
     }
 </script>
+
+<style scoped>
+
+</style>
